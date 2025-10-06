ff842ae99197bc179f0c80365d6220d105ff4a5c
Vladislav Moskalenko <romzivladru@gmail.com>
2025-10-06 17:50:13 +0300

    test ignor

---
+++ Shapes. Number of sides/Shapes. Number of sides/Shapes. Number of sides.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+
+class Address
+{
+private:
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+
+public:
+    Address(const std::string& c, const std::string& s, int h, int a)
+        : city(c), street(s), house(h), apartment(a) {
+    }
+
+    std::string get_output_address() const
+    {
+        return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(apartment);
+    }
+};
+
+
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
